dfefdf5d5661a4673b22132fbdee0155aaf59a3a
Jingnan Si <jingnan.si@gmail.com>
2017-07-19 21:42:22 -0700

    finish first version UI

--- src/main_dlg.cpp
+++ src/main_dlg.cpp
@@ -1,12 +1,14 @@
 #include "main_dlg.h"
 
 MainDialog::MainDialog(const wxString & title)
-    : wxDialog(NULL, -1, title, wxDefaultPosition, wxSize(420, 330))
+    : wxDialog(NULL, -1, title, wxDefaultPosition, wxSize(800, 600))
 {
     wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
 
-    wxSizerFlags sz2Flags(0);
-    sz2Flags.Expand().Center().Border(wxTOP|wxLEFT|wxRIGHT, 5);
+    wxSizerFlags szDefaultFlags(0);
+    szDefaultFlags.Expand().Center().Border(wxTOP|wxLEFT|wxRIGHT, 5);
+    wxSizerFlags szBottomFlags(0);
+    szBottomFlags.Expand().Center().Border(wxALL, 5);
 
    //folder name
     wxStaticBoxSizer *sz = new wxStaticBoxSizer(wxVERTICAL,
@@ -27,8 +29,10 @@ MainDialog::MainDialog(const wxString & title)
                     .Border(wxBOTTOM | wxTOP, 5));
 
     sz->Add(new wxStaticText(sz->GetStaticBox(), wxID_ANY, wxT("Choose folder for batch renaming")),
-            sz2Flags);
-    sz->Add(hboxFolder, sz2Flags);
+            szDefaultFlags);
+    sz->Add(hboxFolder, szDefaultFlags);
+    sz->Add(new wxCheckBox(sz->GetStaticBox(), wxID_ANY, wxT("including sub directories")),
+            szBottomFlags);
 
     //match method
     wxStaticBoxSizer *sz2 = new wxStaticBoxSizer(wxVERTICAL,
@@ -42,12 +46,51 @@ MainDialog::MainDialog(const wxString & title)
                                            wxT("Use Regexp"), wxDefaultPosition);
 
     sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Choose file name matching/replacing method")),
-            sz2Flags);
-    sz2->Add(rb, sz2Flags);
-    sz2->Add(rb1, sz2Flags);
+            szDefaultFlags);
+    sz2->Add(rb, szDefaultFlags);
+    sz2->Add(rb1, szDefaultFlags);
+    sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Source file pattern")),
+            szDefaultFlags);
+    sz2->Add(new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*")),
+            szDefaultFlags);
+    sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Target file pattern")),
+            szDefaultFlags);
+    sz2->Add(new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*")),
+            szBottomFlags);
 
-    vbox->Add(sz, sz2Flags);
-    vbox->Add(sz2, sz2Flags);
+    wxStaticBoxSizer *sz3 = new wxStaticBoxSizer(wxVERTICAL,
+                                                this,
+                                                wxT(""));
+    sz3->Add(new wxStaticText(sz3->GetStaticBox(), wxID_ANY, wxT("File rename preview")),
+            szDefaultFlags);
+
+    wxListCtrl * lcPreview = new wxListCtrl(sz3->GetStaticBox(), wxID_ANY,
+                                            wxDefaultPosition, wxDefaultSize,
+                                            wxLC_REPORT);
+
+    wxListItem itemCol;
+    itemCol.SetText(wxT("Source"));
+    itemCol.SetAlign(wxLIST_FORMAT_CENTRE);
+    lcPreview->InsertColumn(0, itemCol);
+
+    itemCol.SetText(wxT("Target"));
+    itemCol.SetAlign(wxLIST_FORMAT_CENTRE);
+    lcPreview->InsertColumn(1, itemCol);
+
+    lcPreview->SetColumnWidth( 0, wxLIST_AUTOSIZE_USEHEADER );
+    lcPreview->SetColumnWidth( 1, wxLIST_AUTOSIZE_USEHEADER );
+
+    sz3->Add(lcPreview, szBottomFlags);
+
+    wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
+
+    hbox->Add(new wxButton(this, wxID_ANY, wxT("Go")),
+                           wxSizerFlags(1).Center());
+
+    vbox->Add(sz, szDefaultFlags);
+    vbox->Add(sz2, szDefaultFlags);
+    vbox->Add(sz3, szDefaultFlags);
+    vbox->Add(hbox, wxSizerFlags(1).Center());
 
     SetSizer(vbox);
 
--- src/main_dlg.h
+++ src/main_dlg.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <wx/listctrl.h>
 
 class MainDialog : public wxDialog
 {
