08afb66ed0bde6938c4aa0cb13c11d1c8cc7e706
Jingnan Si <jingnan.si@gmail.com>
2017-07-20 22:37:53 -0700

    hook up events

--- src/main_dlg.cpp
+++ src/main_dlg.cpp
@@ -1,5 +1,7 @@
 #include "main_dlg.h"
 
+#include <iostream>
+
 MainDialog::MainDialog(const wxString & title)
     : wxDialog(NULL, -1, title, wxDefaultPosition, wxSize(800, 600))
 {
@@ -10,18 +12,17 @@ MainDialog::MainDialog(const wxString & title)
     wxSizerFlags szBottomFlags(0);
     szBottomFlags.Expand().Center().Border(wxALL, 5);
 
-   //folder name
+    //folder name
     wxStaticBoxSizer *sz = new wxStaticBoxSizer(wxVERTICAL,
                                                 this,
                                                 wxT(""));
 
     wxBoxSizer *hboxFolder = new wxBoxSizer(wxHORIZONTAL);
-    wxTextCtrl *tcFolder = new wxTextCtrl(sz->GetStaticBox(), wxID_ANY, wxT(""),
-                                          wxDefaultPosition, wxDefaultSize);
-    wxButton *folderButton = new wxButton(sz->GetStaticBox(), -1, wxT("Browse"),
-                                      wxDefaultPosition, wxDefaultSize);
+    m_tcFolder = new wxTextCtrl(sz->GetStaticBox(), wxID_ANY, wxT(""));
 
-    hboxFolder->Add(tcFolder,
+    wxButton *folderButton = new wxButton(sz->GetStaticBox(), ID_BTN_BROWSE, wxT("Browse"));
+
+    hboxFolder->Add(m_tcFolder,
                     wxSizerFlags(1).Expand().Border(wxBOTTOM | wxTOP | wxRIGHT, 5));
     hboxFolder->Add(folderButton,
                     wxSizerFlags(0)
@@ -31,61 +32,65 @@ MainDialog::MainDialog(const wxString & title)
     sz->Add(new wxStaticText(sz->GetStaticBox(), wxID_ANY, wxT("Choose folder for batch renaming")),
             szDefaultFlags);
     sz->Add(hboxFolder, szDefaultFlags);
-    sz->Add(new wxCheckBox(sz->GetStaticBox(), wxID_ANY, wxT("including sub directories")),
-            szBottomFlags);
+
+    m_cbIncludeSubDir = new wxCheckBox(sz->GetStaticBox(), wxID_ANY, wxT("including sub directories"));
+    sz->Add(m_cbIncludeSubDir, szBottomFlags);
 
     //match method
     wxStaticBoxSizer *sz2 = new wxStaticBoxSizer(wxVERTICAL,
-                                                this,
-                                                wxT(""));
+                                                 this,
+                                                 wxT(""));
 
-    wxRadioButton *rb = new wxRadioButton(sz2->GetStaticBox(), wxID_ANY,
-                                          wxT("Use Wildcard"), wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
+    m_rbWildcards = new wxRadioButton(sz2->GetStaticBox(), wxID_ANY,
+                                      wxT("Use Wildcard"), wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
 
-    wxRadioButton *rb1 = new wxRadioButton(sz2->GetStaticBox(), wxID_ANY,
-                                           wxT("Use Regexp"), wxDefaultPosition);
+    m_rbRegex = new wxRadioButton(sz2->GetStaticBox(), wxID_ANY,
+                                  wxT("Use Regexp"), wxDefaultPosition);
 
     sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Choose file name matching/replacing method")),
-            szDefaultFlags);
-    sz2->Add(rb, szDefaultFlags);
-    sz2->Add(rb1, szDefaultFlags);
+             szDefaultFlags);
+    sz2->Add(m_rbWildcards, szDefaultFlags);
+    sz2->Add(m_rbRegex, szDefaultFlags);
     sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Source file pattern")),
-            szDefaultFlags);
-    sz2->Add(new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*")),
-            szDefaultFlags);
+             szDefaultFlags);
+
+    m_tcSourcePattern = new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*"));
+    sz2->Add(m_tcSourcePattern, szDefaultFlags);
+
     sz2->Add(new wxStaticText(sz2->GetStaticBox(), wxID_ANY, wxT("Target file pattern")),
-            szDefaultFlags);
-    sz2->Add(new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*")),
-            szBottomFlags);
+             szDefaultFlags);
+
+    m_tcTargetPattern = new wxTextCtrl(sz2->GetStaticBox(), wxID_ANY, wxT("*"));
+    sz2->Add(m_tcTargetPattern, szBottomFlags);
 
     wxStaticBoxSizer *sz3 = new wxStaticBoxSizer(wxVERTICAL,
-                                                this,
-                                                wxT(""));
+                                                 this,
+                                                 wxT(""));
     sz3->Add(new wxStaticText(sz3->GetStaticBox(), wxID_ANY, wxT("File rename preview")),
-            szDefaultFlags);
+             szDefaultFlags);
 
-    wxListCtrl * lcPreview = new wxListCtrl(sz3->GetStaticBox(), wxID_ANY,
-                                            wxDefaultPosition, wxDefaultSize,
-                                            wxLC_REPORT);
+    wxListCtrl * m_lcPreview = new wxListCtrl(sz3->GetStaticBox(), wxID_ANY,
+                                              wxDefaultPosition, wxDefaultSize,
+                                              wxLC_REPORT | wxLC_VIRTUAL);
 
     wxListItem itemCol;
     itemCol.SetText(wxT("Source"));
     itemCol.SetAlign(wxLIST_FORMAT_CENTRE);
-    lcPreview->InsertColumn(0, itemCol);
+    m_lcPreview->InsertColumn(0, itemCol);
 
     itemCol.SetText(wxT("Target"));
     itemCol.SetAlign(wxLIST_FORMAT_CENTRE);
-    lcPreview->InsertColumn(1, itemCol);
+    m_lcPreview->InsertColumn(1, itemCol);
 
-    lcPreview->SetColumnWidth( 0, wxLIST_AUTOSIZE_USEHEADER );
-    lcPreview->SetColumnWidth( 1, wxLIST_AUTOSIZE_USEHEADER );
+    m_lcPreview->SetColumnWidth( 0, wxLIST_AUTOSIZE_USEHEADER );
+    m_lcPreview->SetColumnWidth( 1, wxLIST_AUTOSIZE_USEHEADER );
 
-    sz3->Add(lcPreview, szBottomFlags);
+    sz3->Add(m_lcPreview, szBottomFlags);
 
     wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
-    hbox->Add(new wxButton(this, wxID_ANY, wxT("Go")),
-                           wxSizerFlags(1).Center());
+    hbox->Add(new wxButton(this, wxID_REPLACE, wxT("Go Rename")),
+              wxSizerFlags(1).Center());
 
     vbox->Add(sz, szDefaultFlags);
     vbox->Add(sz2, szDefaultFlags);
@@ -94,8 +99,50 @@ MainDialog::MainDialog(const wxString & title)
 
     SetSizer(vbox);
 
+    //Connect to Events
+    Connect(wxEVT_TEXT, wxCommandEventHandler(MainDialog::OnUpdatePreview));
+    Connect(wxEVT_CHECKBOX, wxCommandEventHandler(MainDialog::OnUpdatePreview));
+    Connect(wxEVT_RADIOBUTTON, wxCommandEventHandler(MainDialog::OnUpdatePreview));
+
+    Connect(wxEVT_BUTTON, wxCommandEventHandler(MainDialog::OnButtonClick));
+
     Centre();
     ShowModal();
 
     Destroy();
+}
+
+void MainDialog::OnUpdatePreview(wxCommandEvent & event)
+{
+    UpdatePreview(m_tcFolder->GetValue(),
+                  m_cbIncludeSubDir->GetValue(),
+                  m_rbWildcards->GetValue(),
+                  m_tcSourcePattern->GetValue(),
+                  m_tcTargetPattern->GetValue());
+}
+
+void MainDialog::OnButtonClick(wxCommandEvent & event)
+{
+    wxButton * btn = dynamic_cast<wxButton*>(event.GetEventObject());
+
+    if (btn->GetId() == ID_BTN_BROWSE)
+    {
+        wxDirDialog dlg(this, wxT("Choose directory for file renaming"), wxT(""),
+                        wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
+
+        dlg.SetPath(m_tcFolder->GetValue());
+
+        if (wxID_OK == dlg.ShowModal())
+        {
+            m_tcFolder->SetValue(dlg.GetPath());
+        }
+    }
+}
+
+void MainDialog::UpdatePreview(const wxString & folder,
+                               bool includeSubDir,
+                               bool useWildcard,
+                               const wxString & srcPattern,
+                               const wxString & targeParttern)
+{
 }
--- src/main_dlg.h
+++ src/main_dlg.h
@@ -8,4 +8,29 @@ class MainDialog : public wxDialog
 public:
   MainDialog(const wxString& title);
 
+private:
+  enum
+  {
+      ID_BTN_BROWSE = wxID_HIGHEST + 1,
+  };
+
+  wxTextCtrl * m_tcFolder;
+  wxTextCtrl * m_tcSourcePattern;
+  wxTextCtrl * m_tcTargetPattern;
+
+  wxCheckBox * m_cbIncludeSubDir;
+
+  wxRadioButton * m_rbWildcards;
+  wxRadioButton * m_rbRegex;
+
+  wxListCtrl * m_lcPreview;
+
+  void OnUpdatePreview(wxCommandEvent & event);
+  void OnButtonClick(wxCommandEvent & event);
+
+  void UpdatePreview(const wxString & folder,
+                     bool includeSubDir,
+                     bool useWildcard,
+                     const wxString & srcPattern,
+                     const wxString & targeParttern);
 };
